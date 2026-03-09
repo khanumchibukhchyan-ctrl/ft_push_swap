9ce8cfa630c33265c873476804491e789a1bf490
Khanum Chibukhchyan <kchibukh@c2r14s1.42yerevan.am>
2026-03-09 16:46:14 +0400

    l

--- main.c
+++ main.c
@@ -1,16 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap.h                                        :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: kchibukh <kchibukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/03/09 16:34:27 by kchibukh          #+#    #+#             */
-/*   Updated: 2026/03/09 16:34:56 by kchibukh         ###   ########.fr       */
+/*   Created: 2026/03/09 16:27:48 by kchibukh          #+#    #+#             */
+/*   Updated: 2026/03/09 16:45:18 by kchibukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PUSH_SWAP
-# define PUSH_SWAP
+#include "push_swap.h"
 
-#endif
+int main(int argc, char **argv)
+{
+    if (argc < 2)
+        return (0);
+    parse_args(argc, argv);
+    init_stack();
+    sort_stack();
+    return (0);
+}
