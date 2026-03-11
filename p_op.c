b365bc7c01bb8b3cda77bee4171b7d0c88b33741
Khanum Chibukhchyan <kchibukh@c2r14s1.42yerevan.am>
2026-03-11 17:39:21 +0400

    q

--- p_op.c
+++ p_op.c
@@ -1,47 +1,37 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   s_op.c                                             :+:      :+:    :+:   */
+/*   p_op.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: kchibukh <kchibukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/03/11 16:50:29 by kchibukh          #+#    #+#             */
-/*   Updated: 2026/03/11 17:35:51 by kchibukh         ###   ########.fr       */
+/*   Created: 2026/03/11 17:12:14 by kchibukh          #+#    #+#             */
+/*   Updated: 2026/03/11 17:31:22 by kchibukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	sa(t_stack **a)
+void	pa(t_stack **a, t_stack **b)
 {
-	t_stack	*first;
-	t_stack	*second;
+	t_stack	**first1;
+	t_stack	**first2;
 
-	if (!a || !*a || !(*a)->next)
+	first1 = *a;
+	first2 = *b;
+	if (!b || !*b)
 		return ;
-	first = *a;
-	second = (*a)->next;
-	first->next = second->next;
-	second->next = first;
-	*a = second;
+	
 }
 
-void	sb(t_stack **b)
+void	pb(t_stack **a, t_stack **b)
 {
-	t_stack	*first;
-	t_stack	*second;
+	t_stack	**first1;
+	t_stack	**first2;
 
-	if (!b || !*b || !(*b)->next)
+	first1 = *a;
+	first2 = *b;
+	if (!a || !*a)
 		return ;
-	first = *b;
-	second = (*b)->next;
-	first->next = second->next;
-	second->next = first;
-	*b = second;
-}
-
-void	ss(t_stack **a, t_stack **b)
-{
-	sa(a);
-	sb(b);
+	
 }
